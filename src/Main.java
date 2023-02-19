c13fbcd2d4a22f83fb22ed39c55b81860277834c
BlackRaven455 <ghostwarrior455@gmail.com>
2023-02-20 00:46:12 +0900

    Ver 0.01

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner();
+        int count = 0;
+        while (count != 4){
+            System.out.println("1 - Add student/n2- Delete student/n3 - List of students/n4 - Exit");
+            scanner.nextInt();
+            switch (count)
+            {
+                case 1:{
+
+                }
+                case 2:{
+
+                }
+                case 3:{
+
+                }
+                case 4:{
+
+                }
+                default:
+                    System.out.println("No options");;
+            }
+
+        }
+        System.out.println("Thank you");
+    }
