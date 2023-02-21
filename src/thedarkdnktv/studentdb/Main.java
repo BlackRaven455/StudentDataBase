edbf8fef0bda70d5019273774d85f3d97a4849b4
TheDarkDnKTv <x22dnk@hotmail.com>
2023-02-22 01:58:32 +0900

    Cleaning

--- src/thedarkdnktv/studentdb/Main.java
+++ src/thedarkdnktv/studentdb/Main.java
@@ -1,3 +1,4 @@
+package thedarkdnktv.studentdb;
 //Цель: создание консольного приложения, которое позволяет пользователю добавлять, удалять и просматривать информацию о студентах.
 //
 //        Функциональные требования:
@@ -15,46 +16,9 @@
 //        - Код приложения должен быть написан с использованием объектно-ориентированного подхода и разбит на классы, отражающие объекты системы (например, класс "Студент").
 //        - Нужно использовать git для контролля версий
 
-import controller.FileControl;
-import controller.Student;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter file name:");
-        String fileName = scanner.next();
-        int menuControl = 0;
-        FileControl file = new FileControl();
-        while (menuControl != 4) {
-            System.out.println("1 - Add student\n2- Delete student\n3 - List of students\n4 - Exit");
-            menuControl = scanner.nextInt();
-            switch (menuControl) {
-                case 1: {
-                    System.out.println("Enter first and family name, specialization, age, year, group number, avaage score:\n ");
-                    Student student = new Student(scanner.next(), scanner.next(), scanner.next(), scanner.nextInt(), scanner.nextInt(), scanner.nextInt(), scanner.nextFloat());
-                    file.FileWrite(student, fileName);
-                    break;
-                }
-                case 2: {
-                    file.FileRead(fileName);
-                    break;
-                }
-                case 3: {
-                    break;
-                }
-                case 4: {
-                    break;
-                }
-                default:
-                    System.out.println("No options");
 
-            }
-
-        }
-        System.out.println("Thank you for using this shity app");
     }
 }
